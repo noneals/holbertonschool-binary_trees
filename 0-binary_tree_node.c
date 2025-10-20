864ebdbc548a00c52f78b6d41d6a68327ec2d217
aziz5g-tech <bogico.sa@gmail.com>
2025-10-20 19:30:40 +0300

    Fix formatting of null check in binary_tree_node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
-if(newNode == NULL)
+if (newNode == NULL)
 exit(EXIT_FAILURE);
 newNode->parent = parent;
 newNode->n = value;
