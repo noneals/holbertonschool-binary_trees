570aef7f7f909b6484ba878d7bea7174443fdf2a
aziz5g-tech <bogico.sa@gmail.com>
2025-10-22 02:47:59 +0300

    Refactor binary_tree_is_perfect function

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,30 @@
 #include "binary_trees.h"
 
 /**
+ * height_for_balance - Measures the height of a binary tree.
+ * @tree: Pointer to the root node of the tree to measure the height.
+ *
+ * Return: 0 If tree is NULL.
+ */
+
+static size_t height_for_balance(const binary_tree_t *tree)
+{
+	size_t left_height = 0, right_height = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left)
+		left_height = height_for_balance(tree->left) + 1;
+
+	if (tree->right)
+		right_height = height_for_balance(tree->right) + 1;
+
+	return (left_height > right_height ? left_height : right_height);
+}
+
+
+/**
  * binary_tree_is_perfect - Checks if a binary tree is perfect.
  * @tree: Pointer to the root node of the tree to check.
  *
@@ -10,7 +34,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	int left_height, right_height;
 
-	/* If tree is NULL, return 0 */
 	if (tree == NULL)
 		return (0);
 
@@ -32,9 +55,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 					binary_tree_is_perfect(tree->right));
 	}
 
-	/* If the node has one child, or if the left and right */
-	/* subtrees are not perfect or don't have the same height, */
-	/* return 0 */
 	return (0);
 
 }
