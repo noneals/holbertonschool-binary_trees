f3319d3e643deb3dc1f715c6daf38f6d89d1cb78
aziz5g-tech <bogico.sa@gmail.com>
2025-12-05 17:10:48 +0300

    Refactor binary_tree_rotate_left for clarity

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,47 +1,34 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_rotate_left - Performs a left-rotation on a binary tree
- * @tree: Pointer to the root node of the tree to rotate
+ * binary_tree_rotate_left - Left-rotates a binary tree.
+ * @tree: A pointer to the root node of the tree to rotate.
  *
- * This function performs a left-rotation on a binary tree. It moves the right
- * child of the root up to become the new root, and the old root becomes the
- * left child of the new root.
- *
- * Return: Pointer to the new root node of the tree once rotated
+ * Return: A pointer to the new root node after rotation.
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	/* Temporary pointer for the new root of the tree after rotation */
-	binary_tree_t *new_root = NULL;
+	binary_tree_t *pivot, *tmp;
 
-	/* If the tree is NULL, return NULL */
-	if (tree != NULL)
+	if (tree == NULL || tree->right == NULL)
+		return (NULL);
+
+	pivot = tree->right;
+	tmp = pivot->left;
+	pivot->left = tree;
+	tree->right = tmp;
+	if (tmp != NULL)
+		tmp->parent = tree;
+	tmp = tree->parent;
+	tree->parent = pivot;
+	pivot->parent = tmp;
+	if (tmp != NULL)
 	{
-		/* Assign the right child of the root to the new_root pointer */
-		new_root = tree->right;
-
-		/* If there is a right child, perform the rotation */
-		if (new_root != NULL)
-		{
-			/* Move left child of new root to be new right child of old root */
-			tree->right = new_root->left;
-
-			/* If left child of new root exists, update its parent to old root */
-			if (new_root->left != NULL)
-				new_root->left->parent = tree;
-
-			/* Move the old root to be the left child of the new root */
-			new_root->left = tree;
-
-			/* Update the parent of the old root to be the parent of the new root */
-			new_root->parent = tree->parent;
-
-			/* Update the parent of the new root to be the old root */
-			tree->parent = new_root;
-		}
+		if (tmp->left == tree)
+			tmp->left = pivot;
+		else
+			tmp->right = pivot;
 	}
 
-	/* Return the new root of the tree after rotation */
-	return (new_root);
+	return (pivot);
 }
