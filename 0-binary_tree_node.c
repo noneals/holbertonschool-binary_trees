f458f94feac4c7a29b89679df9521101945c4f85
aziz5g-tech <bogico.sa@gmail.com>
2025-12-05 17:03:15 +0300

    Refactor binary_tree_node for better memory handling
    Updated the binary_tree_node function to improve error handling and memory allocation.

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,22 +1,25 @@
 #include "binary_trees.h"
 
 /**
-  * binary_tree_node - a function that creates a binary tree node
-  * @parent: a pointer to the parent node of the node to create
-  * @value: the value to put in the new node
-  *
-  * Return: pointer to the new node, or NULL on failure
-  */
+ * binary_tree_node - Creates a binary tree node.
+ * @parent: A pointer to the parent of the node to create.
+ * @value: The value to put in the new node.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new node.
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new;
 
-if (new_node == NULL)
-exit(EXIT_FAILURE);
-new_node->parent = parent;
-new_node->n = value;
-new_node->left = NULL;
-new_node->right = NULL;
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
 
-return (new_node);
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+
+	return (new);
 }
